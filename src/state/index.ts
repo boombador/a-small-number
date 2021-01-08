ad714693e4566700b7a5b52d09ea76c6890e4fbd
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2021-01-07 18:38:02 -0600

    implement some basic events

--- src/state/index.ts
+++ src/state/index.ts
@@ -88,9 +88,74 @@ const emptyResources = {
   wood: 0,
 };
 
-const calculateDaysEvents = (gameState: GameState) => {
-  // TODO: replace hardcoded event with processing for specific activities
-  return [{ message: '[Placeholder event] Gathered Berries', resourceDeltas: { ...emptyResources, food: 10 } }];
+const waterEvent = (gameState: GameState): GameEvent => {
+  const luck = Math.random();
+  const people = gameState.activityAllocations.collect_water;
+  const water = (3 + luck) * people;
+  return {
+    message: `Retrieved water, stored ${water} units`,
+    resourceDeltas: { ...emptyResources, water },
+  };
+};
+
+const forageEvent = (gameState: GameState): GameEvent => {
+  const successProbability = 0.9;
+  const luck = Math.random();
+  const people = gameState.activityAllocations.gather_food;
+  const failed = luck > successProbability;
+  if (failed) {
+    return {
+      message: 'No food was found, hopefully we need less than we thought',
+      resourceDeltas: { ...emptyResources },
+    };
+  }
+  const food = (1.5 + luck) * people;
+  return {
+    message: `Gathered some berries, stored ${food} units`,
+    resourceDeltas: { ...emptyResources, food },
+  };
+};
+
+const huntEvent = (gameState: GameState): GameEvent => {
+  const luck = Math.random();
+  const people = gameState.activityAllocations.hunt;
+  const successProbability = 0.4;
+  const failed = people <= 0 || luck > successProbability;
+  if (failed) {
+    return {
+      message: 'The hunt failed',
+      resourceDeltas: { ...emptyResources },
+    };
+  }
+  const food = (10 + luck * 10) * people;
+  return {
+    message: `Caught some meat, stored ${food} units`,
+    resourceDeltas: { ...emptyResources, food },
+  };
+};
+
+const lumberEvent = (gameState: GameState): GameEvent => {
+  const wood = 0;
+  return {
+    message: `Retrieved lumber, stored ${wood} units`,
+    resourceDeltas: { ...emptyResources, wood },
+  };
+};
+
+// const scoutEvent = (gameState: GameState): GameEvent => {};
+// const defendEvent = (gameState: GameState): GameEvent => {};
+
+const calculateDaysEvents = (gameState: GameState): GameEvent[] => {
+  const result = [];
+
+  result.push(waterEvent(gameState));
+  result.push(forageEvent(gameState));
+  result.push(huntEvent(gameState));
+  result.push(lumberEvent(gameState));
+  // result.push(scoutEvent(gameState));
+  // result.push(defendEvent(gameState));
+
+  return result;
 };
 
 const updatedResources = (
