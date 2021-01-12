0eb43c781f407fadeccabff1fcfd20c571c7b2e7
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2021-01-12 15:31:23 -0600

    Repurpose sample box as base

--- src/app/visualization/Visualization.tsx
+++ src/app/visualization/Visualization.tsx
@@ -1,34 +1,6 @@
-import React, { useRef, useState } from 'react';
-import { Canvas, MeshProps, useFrame } from 'react-three-fiber';
-import type { Mesh } from 'three';
-
-const Box: React.FC<MeshProps> = (props) => {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef<Mesh>();
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-  });
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  );
-};
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import Encampment from './Encampment';
 
 import './Visualization.css';
 
@@ -38,8 +10,7 @@ export const Visualization: React.FunctionComponent = () => {
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <Encampment position={[0, 0, 0]} />
       </Canvas>
     </div>
   );
